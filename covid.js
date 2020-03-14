8251707e00dfe4a50c9a9fec3b5205927a9f99db
Zack M. Davis <code@zackmdavis.net>
2020-03-14 10:58:38 -0700

    run `js_beautify` code-formatter
    At first I was going to try `prettier` (which I think is popular
    nowadays), but I didn't feel like figuring out how to get my `npm`
    environment set up right, so I just apt-got this package.

--- covid.js
+++ covid.js
@@ -1,36 +1,36 @@
 function doBayes() {
     let symptoms = ["fever", "fatigue", "cough", "sneezing", "achesPains", "runnyNose", "soreThroat", "diarrhea", "headache", "shortnessBreath"];
     let diseases = ["covid", "flu", "cold"];
-    let covidOccurence = 1000/60000000;
-    let PA = [0.0000005,0.05,0.002];
-    let PB = [0.023,0.016,0.046,0.0023,0.0029,0.028,0.031,0.01,0.025,0.011];
-    for (let i=0; i < PB.length; i++) {
-        PB[i] = PB[i]/52;
+    let covidOccurence = 1000 / 60000000;
+    let PA = [0.0000005, 0.05, 0.002];
+    let PB = [0.023, 0.016, 0.046, 0.0023, 0.0029, 0.028, 0.031, 0.01, 0.025, 0.011];
+    for (let i = 0; i < PB.length; i++) {
+        PB[i] = PB[i] / 52;
     }
-    let PBFlu = [0.3,0.3,0.3,0,0.3,0.1,0.1,0.1,0.3,0];
-    let PBCold = [0.03,0.1,0.3,0.3,0.3,0.3,0,0.03,0];
-    let PBCovid = [0.879,0.381,0.677,0.001,0.148,0.048,0.139,0.037,0.136,0.186];
+    let PBFlu = [0.3, 0.3, 0.3, 0, 0.3, 0.1, 0.1, 0.1, 0.3, 0];
+    let PBCold = [0.03, 0.1, 0.3, 0.3, 0.3, 0.3, 0, 0.03, 0];
+    let PBCovid = [0.879, 0.381, 0.677, 0.001, 0.148, 0.048, 0.139, 0.037, 0.136, 0.186];
 
     let Psymptomset = 1;
     let Psymptomset_cold = 1;
     let Psymptomset_flu = 1;
     let Psymptomset_covid = 1;
     for (let symptom = 0; symptom < symptoms.length; symptom++) {
-        if (document.getElementById(symptoms[symptom]+"Symptom").checked) {
-            Psymptomset = Psymptomset*PB[symptom]
-            Psymptomset_cold = Psymptomset_cold*PBCold[symptom]
-            Psymptomset_flu = Psymptomset_flu*PBFlu[symptom]
-            Psymptomset_covid = Psymptomset_covid*PBCovid[symptom]
+        if (document.getElementById(symptoms[symptom] + "Symptom").checked) {
+            Psymptomset = Psymptomset * PB[symptom]
+            Psymptomset_cold = Psymptomset_cold * PBCold[symptom]
+            Psymptomset_flu = Psymptomset_flu * PBFlu[symptom]
+            Psymptomset_covid = Psymptomset_covid * PBCovid[symptom]
         } else {
-            Psymptomset = Psymptomset*(1 - PB[symptom])
-            Psymptomset_cold = Psymptomset_cold*(1-PBCold[symptom])
-            Psymptomset_flu = Psymptomset_flu*(1-PBFlu[symptom])
-            Psymptomset_covid = Psymptomset_covid*(1-PBCovid[symptom])
+            Psymptomset = Psymptomset * (1 - PB[symptom])
+            Psymptomset_cold = Psymptomset_cold * (1 - PBCold[symptom])
+            Psymptomset_flu = Psymptomset_flu * (1 - PBFlu[symptom])
+            Psymptomset_covid = Psymptomset_covid * (1 - PBCovid[symptom])
         }
     }
 
     let Pcovid = PA[0] / Psymptomset * Psymptomset_covid;
-    Pcovid = Pcovid/(1 + Pcovid);
+    Pcovid = Pcovid / (1 + Pcovid);
 
-    document.getElementById("diseaseChances").innerHTML = "<p>Coronavirus chance: " + Pcovid*100 + "%" + "</p>"
+    document.getElementById("diseaseChances").innerHTML = "<p>Coronavirus chance: " + Pcovid * 100 + "%" + "</p>"
 }
