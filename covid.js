8eacd65a603c0a8d91d21f94abec3ac527ac41c4
Zack M. Davis <code@zackmdavis.net>
2020-03-14 12:27:11 -0700

    DRY rewrite

--- covid.js
+++ covid.js
@@ -1,43 +1,58 @@
 function doBayes() {
-    let symptoms = ["fever", "fatigue", "cough", "sneezing", "achesPains", "runnyNose", "soreThroat", "diarrhea", "headache", "shortnessBreath"];
-    let diseases = ["covid", "flu", "cold"];
-    let PA = [0.0000005, 0.05, 0.002];
-    let PB = [0.023, 0.016, 0.046, 0.0023, 0.0029, 0.028, 0.031, 0.01, 0.025, 0.011];
-    for (let i = 0; i < PB.length; i++) {
-        // XXX: why are we dividing by 52 here?! I can't figure it out —ZMD
-        PB[i] /= 52;
-    }
-    let PBFlu = [0.3, 0.3, 0.3, 0, 0.3, 0.1, 0.1, 0.1, 0.3, 0];
-    let PBCold = [0.03, 0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0, 0.03, 0];
-    let PBCovid = [0.879, 0.381, 0.677, 0.001, 0.148, 0.048, 0.139, 0.037, 0.136, 0.186];
+    let diseases = ["base", "cold", "flu", "covid"];
+    let diseaseDisplayNames = {
+        cold: "Common cold",
+        flu: "Influenza",
+        covid: "COVID-19",
+    };
 
-    let Psymptomset = 1;
-    let Psymptomset_cold = 1;
-    let Psymptomset_flu = 1;
-    let Psymptomset_covid = 1;
-    for (let symptom = 0; symptom < symptoms.length; symptom++) {
-        if (document.getElementById(symptoms[symptom] + "Symptom").checked) {
-            Psymptomset *= PB[symptom]
-            Psymptomset_cold *= PBCold[symptom]
-            Psymptomset_flu *= PBFlu[symptom]
-            Psymptomset_covid *= PBCovid[symptom]
-        } else {
-            Psymptomset *= 1 - PB[symptom]
-            Psymptomset_cold *= 1 - PBCold[symptom]
-            Psymptomset_flu *= 1 - PBFlu[symptom]
-            Psymptomset_covid *= 1 - PBCovid[symptom]
+    let symptoms = ["fever", "fatigue", "cough", "sneezing", "achesPains", "runnyNose", "soreThroat", "diarrhea", "headache", "shortnessBreath"];
+
+    let prior = {
+        cold: 0.05,
+        flu: 0.002,
+        covid: 0.0000005,
+    };
+
+    let symptomRates = {
+        // XXX: why are we dividing by 52 here?! I can't figure it out —ZMD
+        base: [0.023, 0.016, 0.046, 0.0023, 0.0029, 0.028, 0.031, 0.01, 0.025, 0.011].map(x => x/52),
+        cold: [0.03, 0.1, 0.1, 0.3, 0.3, 0.3, 0.3, 0, 0.03, 0],
+        flu: [0.3, 0.3, 0.3, 0, 0.3, 0.1, 0.1, 0.1, 0.3, 0],
+        covid: [0.879, 0.381, 0.677, 0.001, 0.148, 0.048, 0.139, 0.037, 0.136, 0.186]
+    };
+
+    let likelihood = {
+        base: 1,
+        cold: 1,
+        flu: 1,
+        covid: 1,
+    };
+
+    for (let [symptomNo, symptom] of symptoms.entries()) {
+        for (let disease of diseases) {
+            if (document.getElementById(symptom + "Symptom").checked) {
+                likelihood[disease] *= symptomRates[disease][symptomNo];
+            } else {
+                likelihood[disease] *= 1 - symptomRates[disease][symptomNo];
+            }
         }
     }
 
-    let Pcovid = PA[0] / Psymptomset * Psymptomset_covid;
-    // convert odds ratio to probability?
-    Pcovid = Pcovid / (1 + Pcovid);
+    let el = document.getElementById("diseaseChances");
+    // clear the div if it's been filled from a previous click
+    el.innerHTML = "";
+    for (let disease of diseases) {
+        if (disease === "base") {
+            continue;
+        }
+        let odds = prior[disease] * likelihood[disease] / likelihood["base"];
+        let probability = odds / (1 + odds);
 
-    let Pflu = PA[2] / Psymptomset * Psymptomset_flu;
-    Pflu = Pflu/(1 + Pflu);
+        let p = document.createElement("p");
+        let t = document.createTextNode(`${diseaseDisplayNames[disease]} chance: ${(probability*100).toFixed(2)}%`);
+        p.appendChild(t);
+        el.appendChild(p);
+    }
 
-    let Pcold = PA[1] / Psymptomset * Psymptomset_cold;
-    Pcold = Pcold/(1 + Pcold);
-
-    document.getElementById("diseaseChances").innerHTML = "<p>Coronavirus chance: " + Pcovid*100 + "%" + "</p><p>Flu chance: " + Pflu*100 + "%" + "</p><p>Cold chance: " + Pcold*100 + "%" + "</p>";
 }
